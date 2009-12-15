bfb7fffc7a5abe2a3022cb98f37995b766393340
Jez Humble <jez@india.com>
2009-12-15 22:51:02 +0000

    Implemented cpu usage for Windows. Don't even know if it compiles at this point though.

--- windows/javasysmon.c
+++ windows/javasysmon.c
@@ -2,9 +2,10 @@
  *  javasysmon.c
  *  javanativetools
  *
- *  Created by Jez Humble on 10 Dec 2009.
+ *  Created by Jez Humble on 15 Dec 2009.
  *  Copyright 2009 Jez Humble. All rights reserved.
  *  Licensed under the terms of the New BSD license.
+ *  TODO: Error checking
  */
 #include <jni.h>
 #include <windows.h>
@@ -12,6 +13,8 @@
 
 static SYSTEM_INFO system_info;
 static int num_cpu;
+static LONGLONG p_idle, p_kernel, p_user;
+static float p_cpu_usage;
 
 JNIEXPORT jint JNICALL JNI_OnLoad (JavaVM * vm, void * reserved)
 {
@@ -19,11 +22,39 @@ JNIEXPORT jint JNICALL JNI_OnLoad (JavaVM * vm, void * reserved)
   GetSystemInfo (&system_info);
   num_cpu = system_info.dwNumberOfProcessors;
 
+  // Initialise ticks
+  FILETIME idletime, kerneltime, usertime;
+  GetSystemTimes(&idletime, &kerneltime, &usertime);
+  p_idle = filetime_to_int64(idletime);
+  p_kernel = filetime_to_int64(kerneltime);
+  p_user = filetime_to_int64(usertime);
+  p_cpu_usage = 0;
+
   return JNI_VERSION_1_2;
 }
 
 JNIEXPORT jfloat JNICALL Java_com_jezhumble_javasysmon_WindowsMonitor_cpuUsage (JNIEnv *env, jobject object)
 {
+  LONGLONG idle, kernel, user, total, p_total;
+  float cpu_usage;
+  FILETIME idletime, kerneltime, usertime;
+
+  GetSystemTimes(&idletime, &kerneltime, &usertime);
+  idle = filetime_to_int64(idletime);
+  kernel = filetime_to_int64(kerneltime);
+  user = filetime_to_int64(usertime);
+  total = idle + kernel + user;
+  p_total = p_idle + p_kernel + p_user;
+  if (total == p_total || idle == p_idle) {
+    cpu_usage = p_cpu_usage;
+  } else {
+    cpu_usage = ((float)1) - ((float)(idle - p_idle)) / ((float)(total - p_total));
+    // Reset counters
+    p_idle = idle;
+    p_user = user;
+    p_kernel = kernel;
+  }
+  return (jfloat)cpu_usage;
 }
 
 JNIEXPORT jlong JNICALL Java_com_jezhumble_javasysmon_WindowsMonitor_totalMemory (JNIEnv *env, jobject object)
@@ -62,4 +93,14 @@ JNIEXPORT jint JNICALL Java_com_jezhumble_javasysmon_WindowsMonitor_numCpus (JNI
 JNIEXPORT jlong JNICALL Java_com_jezhumble_javasysmon_WindowsMonitor_cpuFrequency (JNIEnv *env, jobject object)
 {
   return (jlong)0;
+}
+
+static LONGLONG filetime_to_int64 (const FILETIME * filetime)
+{
+    ULARGE_INTEGER time;
+
+    time.LowPart = filetime->dwLowDateTime;
+    time.HighPart = filetime->dwHighDateTime;
+
+    return time.QuadPart;
 }
